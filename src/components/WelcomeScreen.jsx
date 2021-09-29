886de4eeb99f37b33b47270e8be33682f8f13ef6
clecardona <clecardona@gmail.com>
2021-09-29 15:40:50 +0200

    styling

--- src/components/WelcomeScreen.jsx
+++ src/components/WelcomeScreen.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import Button from "./shared/Button";
 import welcomeImg from "../assets/img/welcome.svg";
-import { addTask } from "../utils/task";
 import Modal from "./Modal";
 
 export default function Welcome({ setReload }) {
@@ -13,12 +12,11 @@ export default function Welcome({ setReload }) {
 
       <h2>Welcome!</h2>
       <p>
-        Une to-do list est un procédé qui se veut simple et efficace pour gérer
-        les tâches d'un projet. Ces tâches peuvent être indépendantes ou devoir,
-        au contraire, être accomplies dans un certain ordre. Un chef de projet
-        informatique y notera par exemple les bogues à corriger et les dates de
-        début et de fin de problème. Un dispatcher (autre exemple) y notera les
-        tâches à réaliser au sein de son quart.
+        Oganize your daily routines with Todolist. Free your mind. Have peace of
+        mind by adding all your tasks to your to-do list (wherever you are or
+        whatever device you use). Todolist gives you the confidence that
+        everything’s organized and accounted for, so you can make progress on
+        the things that are important to you.
       </p>
       <Button type="btn btn-main btn-300" onClick={() => setIsOpen(true)}>
         Add a Task
